380138c2de85d110221d1e8494887b90eaaa8132
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-08-22 20:54:11 +0545

    used currency-converter-lt npm package

--- qr.js
+++ qr.js
@@ -1,6 +1,23 @@
-const QRCode = require('qrcode');
-const url = "http://www.nepal.com";
+// const QRCode = require('qrcode');
+// const url = "http://www.nepal.com";
 
-QRCode.toString('I am a carrot!',{type:'terminal'}, function (err, url) {
-  console.log(url)
+// QRCode.toString('I am a carrot!',{type:'terminal'}, function (err, url) {
+// //   console.log(url)
+// })
+
+//
+
+// const QRCode = require('qrcode');
+
+// QRCode.toDataURL('I am Tony!', function (err, url) {
+//   console.log(url)
+// })
+
+//
+
+const CC = require('currency-converter-lt')
+let currencyConverter = new CC()
+
+currencyConverter.from("USD").to("GBP").amount(125).convert().then((response) => {
+    console.log(response) //or do something else
 })
