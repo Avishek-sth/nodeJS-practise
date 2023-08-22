134a8bf3ff7fe15d12b7bcf352df0e4167ffd414
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-08-22 09:02:10 +0545

    used upper-case npm package

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const http = require('http'); //nodejs inbuilt package
 const myModule = require("./myCustMod");
+const upperCase = require("upper-case");
 
 http.createServer((req, res)=>{
     res.writeHead(200, { "content-type": "text/html"}); //html tag
     const date = myModule.myDate();
     const myTime = myModule.myTime();
+    const myName = upperCase.upperCase("Avishek");
     //res.write(`<h1>Hello Avi's World.</h1><p>${date}</p><p>${myTime}</p>`); //body
-    res.write("<h1>Hello World!</h1><p>"+ date +"</p><p>"+ myTime + "</p>");
+    res.write("<h1>Hello World!" + myName + "</h1><p>"+ date +"</p><p>"+ myTime + "</p>");
     res.end();
 }).listen(5555);
 
