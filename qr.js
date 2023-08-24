fd577b688b5fd8161c418f522084da154a1ab255
Avishek Shrestha <avishek.shrestha16@gmail.com>
2023-08-24 13:52:59 +0545

    displayed qr code generated from npm in browser

--- qr.js
+++ qr.js
@@ -7,47 +7,62 @@
 
 //
 
-// const QRCode = require('qrcode');
+const http = require("http");
+const QRCode = require('qrcode');
 
 // QRCode.toDataURL('I am Tony!', function (err, url) {
 //   console.log(url)
 // })
 
+http.createServer(async (req, res)=> {
+    res.writeHead (200, { "content-type": "text/html"});
+    const qrData = await QRCode.toDataURL ("I am not Tony!");
+    res.write(`<img src=${qrData} />`);
+    res.end();
+})
+.listen(3333);
+console.log("App is running.");
 //
 
-// const CC = require('currency-converter-lt')
-// let currencyConverter = new CC()
 
-// currencyConverter.from("USD").to("GBP").amount(125).convert().then((response) => {
-//     console.log(response) //or do something else
-// })
+
+// const PDFDocument = require('pdfkit');
+// const fs = require('fs');
+
+// const doc = new PDFDocument();
+
+// doc.pipe(fs.createWriteStream('output.pdf'));
+
+// doc
+//   .fontSize(25)
+//   .text('Some text with an embedded font!', 100, 100);
+// doc
+//   .addPage()
+//   .fontSize(25)
+//   .text('Here is some vector graphics...', 100, 100);
+// doc
+//   .save()
+//   .moveTo(100, 150)
+//   .lineTo(100, 250)
+//   .lineTo(200, 250)
+//   .fill('#FF3300');
+// doc
+//   .addPage()
+//   .fillColor('blue')
+//   .text('Here is a link!', 100, 100)
+//   .underline(100, 100, 160, 27, { color: '#0000FF' })
+//   .link(100, 100, 160, 27, 'http://google.com/');
+// doc.end();
 
 //
 
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
+//Weather API using package axios and open source weather url LINK
 
-const doc = new PDFDocument();
+// const axios = require("axios");
+// const moment = require("moment");
 
-doc.pipe(fs.createWriteStream('output.pdf'));
-
-doc
-  .fontSize(25)
-  .text('Some text with an embedded font!', 100, 100);
-doc
-  .addPage()
-  .fontSize(25)
-  .text('Here is some vector graphics...', 100, 100);
-doc
-  .save()
-  .moveTo(100, 150)
-  .lineTo(100, 250)
-  .lineTo(200, 250)
-  .fill('#FF3300');
-doc
-  .addPage()
-  .fillColor('blue')
-  .text('Here is a link!', 100, 100)
-  .underline(100, 100, 160, 27, { color: '#0000FF' })
-  .link(100, 100, 160, 27, 'http://google.com/');
-doc.end();
+// async function getWeather(){
+//     try{
+//         const response = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto");
+//     }
+// }
